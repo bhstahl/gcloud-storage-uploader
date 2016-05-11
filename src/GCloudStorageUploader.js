53640526efd82ae8032dd17dac8f93b59f1dc780
Ben Stahl <bhstahl@gmail.com>
2016-05-11 15:43:14 -0400

    Create upload endpoints

--- src/GCloudStorageUploader.js
+++ src/GCloudStorageUploader.js
@@ -2,6 +2,7 @@
 
 const googleAuth = require('google-auto-auth');
 const assign = require('object-assign');
+const request = require('request');
 
 const DEFAULT_CONFIG = {
     keyFilename: 'keyfile.json',
@@ -16,37 +17,61 @@ class GCloudStorageUploader {
 
     /**
      * [_getAuthorization description]
-     * @param  {[type]} options [description]
+     * @param  {[type]} config [description]
      * @return {[type]}         [description]
      */
-    _getAuthorization(options) {
+    _getAuthorization(config) {
+        const fileName = config.fileName || config.file.split('/').pop();
+
         return new Promise((resolve, reject) => {
             const params = {
                 method: 'POST',
-                uri: `https://www.googleapis.com/upload/storage/v1/b/${options.bucket}/o`,
+                uri: `https://www.googleapis.com/upload/storage/v1/b/${config.bucket}/o`,
                 qs: {
-                    name: options.file,
+                    name: fileName,
                     uploadType: 'resumable',
                 },
-                json: options.metadata || {},
+                json: config.metadata || {},
                 headers: {},
             };
 
-            this.authClient.authorizeRequest(params, (error, response) => {
+            this.authClient.authorizeRequest(params, (error, paramsWithAuth) => {
                 if (error) {
                     console.log(error);
-                    reject(error);
-                    return;
+                    return reject(error);
                 }
 
-                resolve(response);
+                return resolve({
+                    config,
+                    paramsWithAuth,
+                });
             });
         });
     }
 
+    /**
+     * [_getEndpoint description]
+     * @param  {[type]} options [description]
+     * @return {[type]}         [description]
+     */
+    _getEndpoint(options) {
+        return new Promise((resolve, reject) => {
+            request(options.requestParams, (error, response, body) => {
+                if (error || response.statusCode !== 200 || !response.headers.location) {
+                    const reason = `${response.statusCode}: ${response.statusMessage}`;
+                    console.log(`[GCloudStorageUploader._getEndpoint] ${reason}`);
+                    return reject(`Failed to create endpoint - ${reason}`);
+                }
 
-    upload(options) {
-        return this._getAuthorization(options)
+                options.config.uri = response.headers.location;
+                return resolve(options.config);
+            });
+        });
+    }
+
+    upload(config) {
+        return this._getAuthorization(config)
+            .then(this._getEndpoint)
             .then(console.log)
             .catch(console.log);
     }
